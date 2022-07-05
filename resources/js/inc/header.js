a768ef49c0f1373fe1dcafcddae0aa76581e01de
TAM Asano <>
2022-07-05 15:13:00 +0900

    test3

--- resources/js/inc/header.js
+++
@@ -1,9 +0,0 @@
-module.exports = function() {
-  const bodyId = document.querySelector('body').getAttribute('id')
-  const headerNav = document.querySelectorAll('#js-header-nav > li > a')
-
-  for (let i = 0; i < headerNav.length; i++) {
-    var el = headerNav[i]
-    if (bodyId === el.dataset.current) el.classList.add('is-current')
-  }
-}
